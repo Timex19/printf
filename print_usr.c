2216df99d4c2ae5e91b49e1ea5647611864db392
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2021-10-05 20:15:27 +0100

    Creating rot and rev functions

--- print_usr.c
+++ print_usr.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/**
+
+/*
  * print_usr - prints a string and values of
  * non-printed chars
  * @arguments: input string
@@ -7,6 +8,7 @@
  * @ibuf: index for buffer pointer
  * Return: number of chars printed
  */
+
 int print_usr(va_list arguments, char *buf, unsigned int ibuf)
 {
 	unsigned char *str;
