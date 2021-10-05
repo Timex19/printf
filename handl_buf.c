b5415329f06ad470c6cfe021b0c2809659ad390a
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2021-10-05 22:11:41 +0100

    Create print char function, and identifier function

--- handl_buf.c
+++ handl_buf.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
-/**
+/*
  * handl_buf - concatenates the buffer characters
  * @buf: buffer pointer
  * @c: charcter to concatenate
  * @ibuf: index of buffer pointer
  * Return: index of buffer pointer.
  */
+
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf)
 {
 	if (ibuf == 1024)
