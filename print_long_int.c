9072517fc5f37b4094a8df008a79f50fa76a3f5c
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2021-10-05 17:34:02 +0100

    Creating functions that prints short and long numbers

--- print_long_int.c
+++ print_long_int.c
@@ -1,11 +1,13 @@
 #include "main.h"
-/**
+
+/*
  * prinlint - prints a long integer
  * @arguments: input string
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed.
  */
+
 int prinlint(va_list arguments, char *buf, unsigned int ibuf)
 {
 	long int int_input;
