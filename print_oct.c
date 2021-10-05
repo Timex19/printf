2abbd59d40383f202361ac71ed417c63ecb3e502
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2021-10-05 17:47:13 +0100

    Create a function that prints in hex, uppercase hex, and octal

--- print_oct.c
+++ print_oct.c
@@ -1,11 +1,13 @@
 #include "main.h"
-/**
+
+/*
  * print_oct - prints decimal number in octal
  * @arguments: input number
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed.
  */
+
 int print_oct(va_list arguments, char *buf, unsigned int ibuf)
 {
 	int int_input, i, isnegative, count, first_digit;
