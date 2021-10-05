05842124b0891e57e4e9da76ce2efa8bba2fb88d
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2021-10-05 17:57:34 +0100

    Create a functions that prints short and long numbers

--- print_short_oct.c
+++ print_short_oct.c
@@ -1,11 +1,13 @@
 #include "main.h"
-/**
+
+/*
  * prinhoct - prints long decimal number in octal
  * @arguments: input number
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed.
  */
+
 int prinhoct(va_list arguments, char *buf, unsigned int ibuf)
 {
 	short int int_input, i, isnegative, count, first_digit;
